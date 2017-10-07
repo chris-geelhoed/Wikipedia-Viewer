18ac1610922a2ba75e0ea9420968d9f922db259b
Chris <chris.geelhoed@gmail.com>
2017-10-07 13:39:18 -0700

    Delete twitterclient.js

--- twitterclient.js
+++
@@ -1,8 +0,0 @@
-//these credentials are needed to call twitter's API
-var Twitter = require('twitter');
-module.exports = new Twitter({
-    consumer_key: 'rJRNaSF7g1yt0VDU9CTj4PEs9',
-    consumer_secret: 'J7UrXgZ9z0hBHrVRFPeojX0Dm4aECveVsIjLiS47VSZkaPzUjm',
-    access_token_key: '828698577652166656-1PjCO021WHm1uTre7zm5DZ99YqXcX38',
-    access_token_secret: '7edkPE5WHrSVcLVvpF8wZ3CKrjF2BpYnGx9IjktEIkNzj'
-});
